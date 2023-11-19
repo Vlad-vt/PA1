a39e8bcb0dc76a60b0fb0a1ee0834ada7df6acb3
Влад <19Titov.vlad.Titov19@gmail.com>
2023-11-19 17:28:19 +0200

    added comments in the code

--- Assets/Scripts/AnaglyphEffect.cs
+++ Assets/Scripts/AnaglyphEffect.cs
@@ -4,50 +4,62 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class AnaglyphEffect : MonoBehaviour
 {
+    // Shader for the anaglyph effect
     public Shader fxShader;
 
+    // Second camera for rendering the right-eye view
     public Camera cam2;
 
+    // Material for applying the anaglyph effect
     private Material mat;
 
+    // Render texture for storing the right-eye view
     private RenderTexture rt;
 
+    // Eye separation parameter for the anaglyph effect
     public float eyeSeparation = 1.0f;
 
+    // Convergence distance parameter for the anaglyph effect
     public float convergenceDistance = 1.0f;
-    
+
     void Start()
     {
+        // Subscribe to events for updating parameters dynamically
         LabParameters.GetInstance().EyeSeparationValueChanged += AnaglyphEffect_EyeSeparationValueChanged;
         LabParameters.GetInstance().FieldOfViewValueChanged += AnaglyphEffect_FieldOfViewValueChanged;
         LabParameters.GetInstance().NearClippingDistanceValueChanged += AnaglyphEffect_NearClippingDistanceValueChanged;
         LabParameters.GetInstance().ConvergenceDistanceValueChanged += AnaglyphEffect_ConvergenceDistanceValueChanged;
+
+        // Initialize convergence distance and eye separation
         mat.SetFloat("_ConvergenceDistance", convergenceDistance);
         eyeSeparation = 1.0f;
         transform.localEulerAngles = Vector3.up * eyeSeparation;
         cam2.transform.localEulerAngles = Vector3.up * -eyeSeparation;
     }
 
+    // Event handler for changes in convergence distance
     private void AnaglyphEffect_ConvergenceDistanceValueChanged(float value)
     {
         convergenceDistance = value;
         mat.SetFloat("_ConvergenceDistance", convergenceDistance);
     }
 
+    // Event handler for changes in near clipping distance
     private void AnaglyphEffect_NearClippingDistanceValueChanged(float value)
     {
         cam2.nearClipPlane = value;
     }
 
+    // Event handler for changes in field of view
     private void AnaglyphEffect_FieldOfViewValueChanged(float value)
     {
         cam2.fieldOfView = value;
     }
 
     /// <summary>
-    /// Тут змінюється параметр eye separation 
+    /// Changes the eye separation parameter.
     /// </summary>
-    /// <param name="value"></param>
+    /// <param name="value">New value for eye separation.</param>
     private void AnaglyphEffect_EyeSeparationValueChanged(float value)
     {
         eyeSeparation = value;
@@ -55,29 +67,33 @@ public class AnaglyphEffect : MonoBehaviour
         cam2.transform.localEulerAngles = Vector3.up * -eyeSeparation;
     }
 
+    // Called when the script is enabled
     private void OnEnable()
     {
-        if(fxShader == null)
+        // Check if the shader is assigned
+        if (fxShader == null)
         {
-            enabled = false; 
+            enabled = false;
             return;
         }
 
+        // Create a material using the shader
         mat = new Material(fxShader);
         mat.hideFlags = HideFlags.HideAndDontSave;
         cam2.enabled = false;
 
+        // Set up the render texture
         int width = Screen.width;
         int height = Screen.height;
-
-
         rt = RenderTexture.GetTemporary(width, height, 8, RenderTextureFormat.Default);
         cam2.targetTexture = rt;
     }
 
+    // Called when the script is disabled
     private void OnDisable()
     {
-        if(mat != null)
+        // Clean up the material and release the render texture
+        if (mat != null)
         {
             DestroyImmediate(mat);
         }
@@ -86,22 +102,26 @@ public class AnaglyphEffect : MonoBehaviour
             rt.Release();
         }
         cam2.targetTexture = null;
-
     }
 
+    // Called when rendering the image
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        // Check if necessary components are available
         if (cam2 == null || rt == null || mat == null)
         {
             enabled = false;
             return;
         }
 
+        // Render the right-eye view using the second camera
         cam2.Render();
         mat.SetTexture("_MainTex2", rt);
-        
+
+        // Apply the anaglyph effect to the final image
         Graphics.Blit(source, destination, mat);
 
+        // Release the render texture
         rt.Release();
     }
 }
--- Assets/Shaders/AnaglyphShader.shader
+++ Assets/Shaders/AnaglyphShader.shader
@@ -2,15 +2,17 @@ Shader"Hidden/AnaglyphShader"
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}
-        _MainTex2 ("2nd Texture", 2D) = "white" {}
-        _ConvergenceDistance ("Convergence Distance", float) = 1 
-
+        _MainTex ("Texture", 2D) = "white" {}       // Main texture for the left eye
+        _MainTex2 ("2nd Texture", 2D) = "white" {} // Texture for the right eye
+        _ConvergenceDistance ("Convergence Distance", float) = 1  // Convergence distance for anaglyph effect
     }
+
     SubShader
     {
         // No culling or depth
-        Cull Off ZWrite Off ZTest Always
+Cull Off
+ZWrite Off
+ZTest Always
 
         Pass
         {
@@ -18,50 +20,53 @@ Shader"Hidden/AnaglyphShader"
             #pragma vertex vert
             #pragma fragment frag
 
-            #include "UnityCG.cginc"
+#include "UnityCG.cginc"
 
-            uniform sampler2D _MainTex;
-            uniform sampler2D _MainTex2;
-            uniform float _ConvergenceDistance;
+uniform sampler2D _MainTex; // Sampler for the left eye texture
+uniform sampler2D _MainTex2; // Sampler for the right eye texture
+uniform float _ConvergenceDistance; // Convergence distance value
 
-            struct appdata
-            {
-                float4 vertex : POSITION;
-                float2 uv : TEXCOORD0;
-            };
+            // Input structure for vertex shader
+struct appdata
+{
+    float4 vertex : POSITION;
+    float2 uv : TEXCOORD0;
+};
 
-            struct v2f
-            {
-                float2 uv : TEXCOORD0;
-                float4 vertex : SV_POSITION;
-            };
+            // Output structure for vertex shader
+struct v2f
+{
+    float2 uv : TEXCOORD0;
+    float4 vertex : SV_POSITION;
+};
 
-            v2f vert (appdata v)
-            {
-                v2f o;
-                o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = v.uv;
-                return o;
-            }
+            // Vertex shader function
+v2f vert(appdata v)
+{
+    v2f o;
+    o.vertex = UnityObjectToClipPos(v.vertex);
+    o.uv = v.uv;
+    return o;
+}
 
-            fixed4 frag (v2f i) : SV_Target
-            {
-                fixed4 sideA = tex2D(_MainTex, i.uv);
-                fixed4 sideB = tex2D(_MainTex2, i.uv);
+            // Fragment (pixel) shader function
+fixed4 frag(v2f i) : SV_Target
+{
+                // Sample colors from the left and right eye textures
+    fixed4 sideA = tex2D(_MainTex, i.uv);
+    fixed4 sideB = tex2D(_MainTex2, i.uv);
     
-    
-                // just invert the colors
-                fixed3 red = fixed3(sideA.r, 0, 0);
+                // Invert the colors for anaglyph effect
+    fixed3 red = fixed3(sideA.r, 0, 0);
+    fixed3 cyan = fixed3(0, sideB.g, sideB.b);
                 
-                fixed3 cyan = fixed3(0, sideB.g, sideB.b);
-                
-                fixed4 col = fixed4(red + cyan, sideA.a);
-                
+                // Combine the inverted colors and apply convergence distance
+    fixed4 col = fixed4(red + cyan, sideA.a);
     
-                col.rgb *= _ConvergenceDistance;
+    col.rgb *= _ConvergenceDistance;
     
-                return col;
-            }
+    return col;
+}
             ENDCG
         }
     }
